2d5268f6a005ce5c662919308af798d5dc02ae64
gooteeiii <90635663+gooteeiii@users.noreply.github.com>
2022-02-13 00:47:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// requires path object, the object is defined in the other .js files
+const path = require('path')
+
+// requires express module installed
+const express = require('express')
+
+// requires body-parser object, defined on other .js pages
+const bodyParser = require('body-parser')
+
+// creates express object named app
+const app = express()
+
+// sets global config value, this is settings the default engine and its location
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+// creates objects with a path defined
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+// not sure
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// defines a location to allow access to files you otherwise could not access
+app.use(express.static(path.join(__dirname, 'public')))
+
+// defines page locations
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+// defines response for a url error 404
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' })
+})
+
+// listens on port 3000
+app.listen(3000)
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This project is a work in progress for the WDD3600 course at ClarkState. Using vides from The Complete Guide to Node.js, the provided videos will expand this project over time into a functioning website using node.js and its modules.
