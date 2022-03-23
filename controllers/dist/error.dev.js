6390a41da197a1f9dbcbfb2caf26b83b4b1fa728
Alex2point0 <18444027+Alex2point0@users.noreply.github.com>
2022-03-22 21:04:36 -0400

    Deleted dist directory

--- controllers/dist/error.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-exports.get404 = function (req, res, next) {
-  res.status(404).render('404', {
-    pageTitle: 'Page Not Found',
-    path: '/404'
-  });
-};
-//# sourceMappingURL=error.dev.js.map
