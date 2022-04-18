5a984e234b688667abf18612ad4d919b513327d0
Albert Gootee <gootea7787@clarkstate.edu>
2022-04-17 21:38:49 -0400

    added comments

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,3 +1,4 @@
+// middleware that performs a check for every rendered page to determine if user is logged in.
 module.exports = (req, res, next) => {
     if (!req.session.isLoggedIn) {
         return res.redirect('/login')
